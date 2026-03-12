3a5dea89d42fd26e061f9c38900bb7a3c6ccc850
jacobwright32 <jacoblouiswright@gmail.com>
2026-03-12 07:56:41 +0000

    IRLS: increase to 5 iterations

--- price.py
+++ price.py
@@ -134,7 +134,7 @@ class PricingModel:
         try:
             # IRLS: 2 rounds of reweighted least squares for robustness
             w = np.ones(len(ivs))
-            for _ in range(3):
+            for _ in range(5):
                 Xw = X * w[:, None]
                 yw = ivs * w
                 coeffs, _, _, _ = np.linalg.lstsq(Xw, yw, rcond=None)
