1d00b82d86d274ccf8a8f61329ee2a5710c8891d
jacobwright32 <jacoblouiswright@gmail.com>
2026-03-12 09:09:37 +0000

    Tune IRLS Cauchy to 1.5 MAD (MAPE 0.20%)

--- price.py
+++ price.py
@@ -133,7 +133,7 @@ class PricingModel:
                 coeffs, _, _, _ = np.linalg.lstsq(Xw, yw, rcond=None)
                 resid = y_sub - X_sub @ coeffs
                 mad = np.median(np.abs(resid)) + 1e-8
-                w = 1.0 / (1.0 + (resid / (2.0 * mad)) ** 2)
+                w = 1.0 / (1.0 + (resid / (1.5 * mad)) ** 2)
             return coeffs
 
         fitted_vol = np.copy(ivs)
